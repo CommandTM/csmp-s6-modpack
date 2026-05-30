e10e53db9e9658c59788a88c59b7fdd0d39bcae2
CommandTM <117131509+CommandTM@users.noreply.github.com>
2026-05-30 16:46:06 -0400

    Actually Butter frfr

--- overrides/kubejs/server_scripts/customrecipegui/generated/butter.js
+++ overrides/kubejs/server_scripts/customrecipegui/generated/butter.js
@@ -1,10 +1,3 @@
 ServerEvents.recipes(event => {
-  event.recipes.farmersdelight.cooking(
-        "misc", // recipe book tab - valid values: meals, drinks, misc
-        ["oaksdelight:cheese"],
-        "oaksdelight:butter", // output
-        1, // exp
-        10, // cookTime
-        "minecraft:paper" // container
-    )
+  event.smelting('oaksdelight:butter', 'oaksdelight:cheese')
 })
